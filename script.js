fbb54a34bb50c51bdae0a2af7f7624375585b29d
jemstedt <andreas.jemstedt@gmail.com>
2023-07-19 20:15:00 +0000

    update 17

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const navLinks = document.querySelectorAll('nav ul li a');
     const containers = document.querySelectorAll('.container');
+
+    // Function to hide all sections
+    function hideAll() {
+        containers.forEach((container) => {
+            container.style.display = 'none';
+        });
+    }
+
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
-            containers.forEach(container => {
-                container.classList.add('hidden');
-            });
+            hideAll(); // Hide all sections
+            document.getElementById(this.getAttribute('onclick').split("'")[1]).style.display = 'block'; // Show clicked section
             navLinks.forEach(link => link.classList.remove('active'));
             this.classList.add('active');
-            document.querySelector(this.getAttribute('href')).classList.remove('hidden');
         });
     });
+
+    // Display home section by default on page load
+    hideAll();
+    document.getElementById('home').style.display = 'block';
+    document.querySelector('nav ul li a').classList.add('active');
 });
