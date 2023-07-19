84651b095b154105770594ad846d2fe1c9efa49e
jemstedt <andreas.jemstedt@gmail.com>
2023-07-19 19:25:14 +0000

    update 12

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('nav ul li a, nav div a');
+    const navLinks = document.querySelectorAll('nav ul li a');
+    const containers = document.querySelectorAll('.container');
     navLinks.forEach(link => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            containers.forEach(container => {
+                container.classList.add('hidden');
+            });
             navLinks.forEach(link => link.classList.remove('active'));
             this.classList.add('active');
+            document.querySelector(this.getAttribute('href')).classList.remove('hidden');
         });
     });
 });
