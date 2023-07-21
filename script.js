1e1c3d2b7e3cbe27191b50f3b2e400c38a9fd2a0
jemstedt <andreas.jemstedt@gmail.com>
2023-07-21 17:34:14 +0000

    test

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    const containers = document.querySelectorAll('.container');
-    const homeButtons = document.querySelectorAll('.home-buttons button');
+$(function() {
+    // default page to load
+    $('#content').load('home.html');
 
-    // Function to hide all sections
-    function hideAll() {
-        containers.forEach((container) => {
-            container.style.display = 'none';
-        });
-    }
-
-    function addClickListenerToButtons(buttons) {
-        buttons.forEach(button => {
-            button.addEventListener('click', function(event) {
-                event.preventDefault();
-                hideAll(); // Hide all sections
-                document.getElementById(this.getAttribute('onclick').split("'")[1]).style.display = 'block'; // Show clicked section
-                navLinks.forEach(link => link.classList.remove('active'));
-                navLinks.forEach(link => {
-                    if (link.getAttribute('onclick') === this.getAttribute('onclick')) {
-                        link.classList.add('active');
-                    }
-                });
-            });
-        });
-    }
-
-    // Add click listeners to nav links and home buttons
-    addClickListenerToButtons(navLinks);
-    addClickListenerToButtons(homeButtons);
-
-    // Display home section by default on page load
-    hideAll();
-    document.getElementById('home').style.display = 'block';
-    document.querySelector('nav ul li a').classList.add('active');
+    $('nav a').click(function(e) {
+        e.preventDefault();
+        let page = $(this).attr('href');
+        $('#content').load(page);
+        $('nav a').removeClass('active');
+        $(this).addClass('active');
+    });
 });
